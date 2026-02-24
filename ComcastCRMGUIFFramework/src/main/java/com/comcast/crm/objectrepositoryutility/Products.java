3804cf261d9a764d8882541c4c9d8db7dfe6a79c
hp <hp@DESKTOP-BE5AN9G>
2026-02-24 11:54:54 +0530

    Added new element contact to POM Cls

--- ComcastCRMGUIFFramework/src/main/java/com/comcast/crm/objectrepositoryutility/Products.java
+++ ComcastCRMGUIFFramework/src/main/java/com/comcast/crm/objectrepositoryutility/Products.java
@@ -8,6 +8,6 @@ public class Products
 	@FindBy(xpath="//input[@alt='Create Product...']")
 	private WebElement createProductImg;
 	
-	@FindBy(name="search")
-	private WebElement ele3;
+	@FindBy(xpath="//input[@alt='Create Contact...']")
+	private WebElement createContactImg;
 }
